6f5b34d745c39648adf007cead24423a18e67dfb
Valeriy Koryakin <Ranguitar@mail.ru>
2025-11-03 14:35:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -962,7 +962,12 @@ def game_loop():
 
 
 if __name__ == "__main__":
+    import eventlet
+    eventlet.monkey_patch()
+
     port = int(os.environ.get("PORT", 5000))
+    print(f"🚀 Starting Spike.io server on port {port}")
     socketio.start_background_task(game_loop)
     socketio.run(app, host="0.0.0.0", port=port)
+
 
